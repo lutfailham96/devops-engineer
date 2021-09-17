7b0d21229af0f1f7594536e7021d751869666473
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-18 00:52:10 +0700

    Add auth validation & controller

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const validator = require('../validations');
+const authValidation = require('../validations/authValidation');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/register');
-router.post('/login');
+router.post('/register', validator.body(authValidation.registerSchema), authController.register);
+router.post('/login', validator.body(authValidation.loginSchema), authController.login);
 
 module.exports = router;
