0c5d7af8426acd8d0f15f231e8327d831b05b215
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-18 01:08:16 +0700

    Add auth unit testing

---
+++ tests/unit/controllers/authController.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const httpStatus = require('http-status');
+const app = require('../../../src/app');
+
+describe('Test auth routes', () => {
+  const defaultUser = {
+    identity: 'lutfailham96@gmail.com',
+    password: 'mytens999',
+  }
+
+  const defaultNewUser = {
+    email: 'lutfailham96@gmail.com',
+    password: 'mytens999',
+    fullname: 'Ilham L.',
+  }
+
+  test('Login', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('Content-type', 'application/json')
+      .send(defaultUser);
+    expect(response.statusCode).toBe(httpStatus.OK);
+  });
+
+  test('Register', async () => {
+    const response = await request(app)
+      .post('/auth/register')
+      .set('Content-type', 'application/json')
+      .send(defaultNewUser);
+    expect(response.statusCode).toBe(httpStatus.CREATED);
+  });
+});
