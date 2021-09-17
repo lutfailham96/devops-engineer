e7c12dc48f6832d5fd919e5334e09e3ce3c5fa5b
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-18 00:51:31 +0700

    Remove unneeded min character rule

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const registerSchema = Joi.object({
-  email: Joi.string().email().required(),
-  fullname: Joi.string().min(4).max(32).required(),
-  password: Joi.string().min(6).max(16).required(),
+  email: Joi.string().email().max(64).required(),
+  fullname: Joi.string().max(64).required(),
+  password: Joi.string().max(32).required(),
 });
 
 const loginSchema = Joi.object({
-  identity: Joi.string().min(4).max(12).required(),
-  password: Joi.string().min(6).max(16).required(),
+  identity: Joi.string().max(64).required(),
+  password: Joi.string().max(32).required(),
 });
 
 module.exports = {
