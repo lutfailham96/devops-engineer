805a8f79b10a42c75ff093ef429b57e2829c45ef
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-18 01:16:07 +0700

    Add incorect input test

--- tests/unit/controllers/authController.test.js
+++ tests/unit/controllers/authController.test.js
@@ -14,7 +14,15 @@ describe('Test auth routes', () => {
     fullname: 'Ilham L.',
   }
 
-  test('Register', async () => {
+  test('Register - incorect input', async () => {
+    const response = await request(app)
+      .post('/auth/register')
+      .set('Content-type', 'application/json')
+      .send({ username: 'incorect_user' });
+    expect(response.statusCode).toBe(httpStatus.BAD_REQUEST);
+  });
+
+  test('Register - correct input', async () => {
     const response = await request(app)
       .post('/auth/register')
       .set('Content-type', 'application/json')
@@ -22,7 +30,15 @@ describe('Test auth routes', () => {
     expect(response.statusCode).toBe(httpStatus.CREATED);
   });
 
-  test('Login', async () => {
+  test('Login - invalid credential', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .set('Content-type', 'application/json')
+      .send({ identity: 'incorect@gmail.com', password: '12341213' });
+    expect(response.statusCode).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  test('Login - correct credential', async () => {
     const response = await request(app)
       .post('/auth/login')
       .set('Content-type', 'application/json')
