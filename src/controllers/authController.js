973fd9ba83d2dc608cccfa93848578a438775502
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-09-18 00:53:02 +0700

    Fix default identity & error handling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,22 @@
 const httpStatus = require('http-status');
 
 const defaultUser = {
-  identity: 'ilham',
+  identity: 'lutfailham96@gmail.com',
   password: 'mytens999',
   fullname: 'Ilham L.',
 };
 
 const login = async (req, res) => {
   const { identity, password } = req.body;
-  if (identity !== defaultUser.identity && password !== defaultUser.password) {
-    res.sendWrapped('Invalid credential.', httpStatus.UNAUTHORIZED);
+  if (identity !== defaultUser.identity || password !== defaultUser.password) {
+    return res.sendWrapped('Invalid credential.', httpStatus.UNAUTHORIZED);
   }
-  res.sendWrapped('Login success.', httpStatus.OK);
+  return res.sendWrapped('Login success.', httpStatus.OK);
 };
 
 const register = async (req, res) => {
   const { email, fullname, password } = req.body;
-  res.sendWrapped({
+  return res.sendWrapped({
     email,
     fullname,
     password,
